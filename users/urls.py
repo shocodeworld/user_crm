c187e77ff83c0b3f9d560da1370fe7af075ff2be
shocodeart <shocodeart@gmail.com>
2022-11-12 17:46:14 +0500

    add url names

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path("", views.UserListView.as_view()),
-    path("<pk>", views.UserDetailView.as_view()),
-    path("create/", views.users_create),
+    path("", views.UserListView.as_view(), name="users-list"),
+    path("<pk>", views.UserDetailView.as_view(), name="users-detail"),
+    path("create/", views.users_create, name="users-create"),
 ]
