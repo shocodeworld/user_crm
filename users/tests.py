5972bdcb7be618d1d62b4cdaa60683f576ab14c7
shocodeart <shocodeart@gmail.com>
2022-11-22 17:15:29 +0500

    refactor tests

--- users/tests.py
+++ users/tests.py
@@ -10,21 +10,18 @@ class TestUsersSearch(TestCase):
         User.objects.create(username="aziza", first_name="Aziza")
         User.objects.create(username="kamila", first_name="Kamila")
         User.objects.create(username="manizha", first_name="Manizha")
+        self.url = reverse("users-search")
+        self.client = Client()
 
     def test_search_endpoint(self):
-        url = reverse("users-search")
-
-        client = Client()
-        response = client.get(url)
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "users/users_list.html")
 
     def test_search_endpoint_response_users(self):
         query = "Kamila"
-        url = (
-            reverse("users-search") + "?search=" + query
-        )  # users/search/?search=Kamila
-        client = Client()
-        response = client.get(url)
+        url = self.url + "?search=" + query  # users/search/?search=Kamila
+
+        response = self.client.get(url)
         self.assertEqual(response.context["object_list"].count(), 1)
