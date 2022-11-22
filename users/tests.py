b70cbcc29fe014805ff365ff21632562a8362c8c
shocodeart <shocodeart@gmail.com>
2022-11-22 18:39:58 +0500

    add tests for the update, delete, create enpoints

--- users/tests.py
+++ users/tests.py
@@ -86,3 +86,76 @@ class TestUsersCreateView(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "users/users_update.html")
+
+
+class TestUsersUpdateView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username="aziza", first_name="Aziza")
+        self.url = reverse("users-update", args=[self.user.pk])
+
+    def test_get_endpoint(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/users_update.html")
+
+    def test_post_endpoint_success(self):
+        data = {
+            "username": "aziza",
+            "first_name": "Azizajon",
+            "last_name": "Sabirova",
+            "email": "anna@email.com",
+            "is_active": True,
+            "groups": [],
+        }
+        response = self.client.post(self.url, data)
+
+        self.user.refresh_from_db()
+
+        self.assertEqual(self.user.first_name, data["first_name"])
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_endpoint_fail(self):
+        data = {"username": "", "first_name": "Azizajon", "last_name": "", "email": ""}
+        response = self.client.post(self.url, data)
+
+        self.user.refresh_from_db()
+
+        self.assertNotEqual(self.user.first_name, data["first_name"])
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/users_update.html")
+
+    def test_404_on_user_does_not_exist(self):
+        response = self.client.get(reverse("users-update", args=[999999999]))
+
+        self.assertEqual(response.status_code, 404)
+
+
+class TestUsersDeleteView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username="aziza", first_name="Aziza")
+        self.url = reverse("users-delete-confirmation", args=[self.user.pk])
+
+    def test_get_endpoint(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/users_delete.html")
+
+    def test_404_on_user_does_not_exist(self):
+        response = self.client.get(
+            reverse("users-delete-confirmation", args=[999999999])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_endpoint_success(self):
+        data = {}
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, 302)
+
+        assert not User.objects.filter(pk=self.user.pk).first()
