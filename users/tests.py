20980bddbca9d7ae4504a72b45b660ccac8ce637
shocodeart <shocodeart@gmail.com>
2022-11-22 18:03:13 +0500

    add create endpoints tests

--- users/tests.py
+++ users/tests.py
@@ -33,7 +33,56 @@ class TestUsersDetailView(TestCase):
         self.client = Client()
 
     def test_detail_endpoint(self):
-        response = self.client.get(reverse("users-detail", args=[self.user.pk]))
+        url = reverse("users-detail", kwargs={"pk": self.user.pk})
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "users/users_detail.html")
+
+
+class TestUsersListView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("users-list")
+
+    def test_list_endpoint(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/users_list.html")
+
+
+class TestUsersCreateView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse("users-create")
+
+    def test_get_create_endpoint(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/users_update.html")
+
+    def test_post_create_endpoint_success(self):
+        data = {
+            "username": "sfdsodfsf",
+            "first_name": "Anna",
+            "last_name": "Sabirova",
+            "email": "anna@email.com",
+            "is_active": True,
+            "groups": [],
+        }
+        response = self.client.post(self.url, data=data)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_create_endpoint_unccorect_data(self):
+        data = {
+            "username": "",
+            "first_name": "",
+            "last_name": "",
+        }
+        response = self.client.post(self.url, data=data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/users_update.html")
